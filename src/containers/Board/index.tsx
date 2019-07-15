2c232512e886c49ec8b0c823f557764d389d2026
Mark Wilson <mwilson@cibotechnologies.com>
2019-07-15 08:17:38 -0500

    fix whitespace in board

--- src/containers/Board/index.tsx
+++ src/containers/Board/index.tsx
@@ -1,39 +1,35 @@
-import * as React from 'react';
-import Square from '../../components/Square';
-
+import * as React from "react";
+import Square from "../../components/Square";
 
 interface BoardProps {
-    squares: number[];
-    onClick(i: number): void;
-    boardSize: number;
+  squares: number[];
+  onClick(i: number): void;
+  boardSize: number;
 }
-
 
 const Board = function(props: BoardProps) {
-    const renderSquare = function(i: number) {
-        return (
-            <Square
-                key={i}
-                value={props.squares[i]}
-                onClick={() => props.onClick(i)}
-            />
-        );
-    }
+  const renderSquare = function(i: number) {
+    return (
+      <Square
+        key={i}
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
+      />
+    );
+  };
 
-        const boardSize = props.boardSize
-        return (
-            <div>
-                {
-                    [...(Array<string>(boardSize) as any).keys()].map((i) =>
-                        <div key={"boardRow" + i} className="board-row">
-                            {
-                                [...(Array<string>(boardSize) as any).keys()].map((j) => renderSquare(j + i * boardSize))
-                            }
-                        </div>
-                    )
-                }
-            </div>
-        );
-}
+  const boardSize = props.boardSize;
+  return (
+    <div>
+      {[...(Array<string>(boardSize) as any).keys()].map(i => (
+        <div key={"boardRow" + i} className="board-row">
+          {[...(Array<string>(boardSize) as any).keys()].map(j =>
+            renderSquare(j + i * boardSize)
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Board;
